efd7b96c888de7054909240ac267452c57d74ac9
Sippora04 <sippora.toppo95@gmail.com>
2020-10-26 23:29:35 +0530

    Added: Gambler Win or Loose

--- GamblingSimulationProblem.java
+++ GamblingSimulationProblem.java
@@ -1,8 +1,23 @@
 package com.gamblingsimulation;
 
+import java.util.Random;
+
 public class GamblingSimulationProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" ***** Welcome to Gambling Simulation Problem ***** ");
+		
+		//UC1: As a Gambler, would start with a stake of $100 every day and bet$1 every game.
+		int stake = 100;
+		int bet = 1;
+		System.out.println("Stake: " + stake + "\nBet: " + bet);
+		
+		//UC2: As a Gambler make $1 bet so either win or loose $1
+		Random random = new Random();
+		int check = random.nextInt(2);
+		if (check == 1)
+			System.out.println("WIN");
+		else
+			System.out.println("LOOSE");
 	}
 }
