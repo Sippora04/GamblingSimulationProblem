7da8cd4420c9c8e4472c483b0f4925dea9da77e8
Sippora04 <sippora.toppo95@gmail.com>
2020-10-26 23:22:03 +0530

    Added: Gambler stake and bet value

--- GamblingSimulationProblem.java
+++ GamblingSimulationProblem.java
@@ -4,5 +4,8 @@ public class GamblingSimulationProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" ***** Welcome to Gambling Simulation Problem ***** ");
+		int stake = 100;
+		int bet = 1;
+		System.out.println("Stake: " + stake + "\nBet: " + bet);
 	}
 }
