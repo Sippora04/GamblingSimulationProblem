22db83a4d99199af0ccea9af02fc6e8e1cab344b
Sippora04 <sippora.toppo95@gmail.com>
2020-10-26 23:17:30 +0530

    Added: Welcome Message

---
+++ GamblingSimulationProblem.java
@@ -0,0 +1,8 @@
+package com.gamblingsimulation;
+
+public class GamblingSimulationProblem {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" ***** Welcome to Gambling Simulation Problem ***** ");
+	}
+}
